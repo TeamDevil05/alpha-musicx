9bbf2a122170534ebfa23d62848b58ed912b2deb
Monu Gupta <monugupta0126@gmail.com>
2026-05-07 10:35:54 +0530

    Update keep_alive.py

--- ShrutixMusic/keep_alive.py
+++ ShrutixMusic/keep_alive.py
@@ -5,11 +5,13 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is Alive!"
+    return "I am alive"
 
 def run():
+    # यहाँ port=8080 होना बहुत ज़रूरी है
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
+    
